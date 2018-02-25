88467df56fb4d8533d363fbe11210e31f02983c8
Viceriel <viceriel@gmail.com>
2018-02-25 17:30:43 +0100

    Footer

--- src/view.js
+++ src/view.js
@@ -20,12 +20,12 @@ class Main
      */
     view()
     {
-        let main = m("main", {class: "container"},
-                     [m("h1[id=title]", {class: "text-center"}, "Family calculator"),
-                      m("button", {class: "btn btn-outline-success"}, "Calculate"),
-                      m("div", {class: "row text-center"}, [m("div", {class: "col-xl-4 col-lg-4 col-md-6 col-12"}, m("h2", "Income")),
-                                                            m("div", {class: "col-xl-4 col-lg-4 col-md-6 col-12"}, m("h2", "Spents")),
-                                                            m("div", {class: "col-xl-4 col-lg-4 col-md-6 col-12"}, m("h2", "Investments"))])]);
+        let main = m("div", [m("main", {class: "container"}, [m("h1[id=title]", {class: "text-center"}, "Family calculator"),
+                                                    m("div", {class: "row text-center"}, [m("div", {class: "col-xl-4 col-lg-4 col-md-6 col-12"}, m("h2", "Income")),
+                                                                                          m("div", {class: "col-xl-4 col-lg-4 col-md-6 col-12"}, m("h2", "Spents")),
+                                                                                          m("div", {class: "col-xl-4 col-lg-4 col-md-6 col-12"}, m("h2", "Investments"))])]),
+                            m("footer", {class: "container-fluid text-center"}, [m("h3","Nič sa nezdá byť drahé na úver"),
+                                                                                 m("div", {class: "col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 lead text-left"}, "Project serves for family financial planning. There is also effort for creating neural family finance predictor. In case of problems, please contact me at viceriel@gmail.com.")])]);                                                                
         return main;
     }
 }
