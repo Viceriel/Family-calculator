a51fd60caade083a091e08dcd4764032edc9d30a
Viceriel <viceriel@gmail.com>
2018-03-14 17:33:04 +0100

    Commit experiment

--- src/addview.js
+++ src/addview.js
@@ -24,7 +24,8 @@ class AddView
   {
     let m = this._m;
     let main = [m("main", {class: "begin container"}, [m("h3","Add income:"),
-                                                       m("input[type=text],[placeholder=Size of income]", "Name")]),
+                                                       m("input[type=text],[placeholder=Size of income]", "Name"),
+                                                       m("button",{class: "btn btn-outline-success"}, "Confirm")]),
                                                        m("footer", {class: "container-fluid text-center"}, [m("h3","Nič sa nezdá byť drahé na úver"),
                                                                                                             m("div", {class: "col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 lead text-left"}, "Project serves for family financial planning. In case of problems, please contact me at viceriel@gmail.com.")])];
     return main;
