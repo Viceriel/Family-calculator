a683bef7ceecd42643943a944ddbb40165491c5e
Viceriel <viceriel@gmail.com>
2018-03-18 13:21:21 +0100

    Addview append content

--- src/addview.js
+++ src/addview.js
@@ -9,6 +9,23 @@ class AddView
   {
     this._parent = parent;
     this._m = m;
+    this._button = this._m("button", {class: "btn btn-outline-success", onclick: this.removeBtn.bind(this)}, "Confirm");
+    this._row = this._m("div", {class: "row text-center"}, [this._m("input[type=text],[placeholder=Value of spents]"),
+                                                            this._m("select", [m("option", "Year"),
+                                                                               m("option", "Month")]),
+                                                            this._m("input[type=text],[placeholder=Modifier]"),                   
+                                                            this._button]);
+  }
+
+  removeBtn()
+  {
+    let btn = document.getElementsByTagName("button")[0];
+    let par = btn.parentNode;
+    par.removeChild(btn);
+    par = par.parentNode;
+    let div = document.createElement("div");
+    par.parentNode.appendChild(div);
+    this._m.render(par.nextSibling, this._row);
   }
 
   onbeforeremove(vnode)
@@ -24,8 +41,7 @@ class AddView
   {
     let m = this._m;
     let main = [m("main", {class: "begin container"}, [m("h3","Add income:"),
-                                                       m("input[type=text],[placeholder=Size of income]", "Name"),
-                                                       m("button",{class: "btn btn-outline-success"}, "Confirm")]),
+                                                       m("div", this._row)]),
                                                        m("footer", {class: "container-fluid text-center"}, [m("h3","Nič sa nezdá byť drahé na úver"),
                                                                                                             m("div", {class: "col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 lead text-left"}, "Project serves for family financial planning. In case of problems, please contact me at viceriel@gmail.com.")])];
     return main;
