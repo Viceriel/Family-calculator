b6c7e683d970256dfb104521c9123efd72e0bac9
Viceriel <viceriel@gmail.com>
2018-05-26 13:15:50 +0200

    Basics of compute engine

--- src/js/computeEngine.js
+++ src/js/computeEngine.js
@@ -12,14 +12,37 @@ class ComputeEngine
     */
    constructor(range, savings, data, noise)
    {
+       if (isNaN(parseInt(range)))
+       {
+           this._valid = false;
+           this._error = "Wrong range.";
+       }
        this._range = range;
+
+       if (!data)
+       {
+         this._valid = false;
+         this._error = "No items provided!";
+       }
        this._data = data;
+
+       if (isNaN(parseInt(range)))
+       {
+         this._valid = false;
+         this._error = "Wrong savings provided!";
+       }
        this._savings = savings;
        this._noise = noise;
        this._year_ratio = 0;
        this._month_ratio = 0;
        this._integrated_incomes = [];
        this._integrated_investments = [];
+       this._integrated_wealth = [];
+
+       let len = this._data.investment.length;
+       for (let i = 0; i < len; i++)
+            this._integrated_investments[i] = [];
+
    }
 
    get IntegratedIncomes()
@@ -30,6 +53,162 @@ class ComputeEngine
    get IntegratedInvestments()
    {
        return this._integrated_investments;
+   }
+
+   get IntegratedWhealth()
+   {
+      return this._integrated_wealth;
+   }
+
+   /**
+    * Integrate all income based on spends, incomes and investments
+    */
+   integrate()
+   {
+       let size = this._range * 12;
+       this.integrations(size);
+   }
+
+   integrations(size)
+   {
+       let batch = this._savings;
+       let invest_year = 0;
+       for(let i = 1; i <= size; i++)
+       {
+           let index = i - 1;
+           let increment = this.computeBatch("income", "Month") - this.computeBatch("spend", "Month") - this.computeMonthInvest(i);
+           if( i % 12 == 0)
+           {
+             increment += this.computeBatch("income", "Year") - this.computeBatch("spend", "Year") - this.computeYearInvest(i);
+             invest_year = this.integrateInvest("Year", (i/12)-1);
+           }
+           batch += increment;
+           this._integrated_incomes[index] = batch;
+           this._integrated_wealth[index] = batch + this.integrateInvest("Month", index) + invest_year;
+       }
+   }
+
+   /**
+    * integrate invest value based on scale
+    *
+    * @param  {string} scale time scale
+    * @param  {Number} index
+    * @return {Num}       value of integrated investment
+    */
+   integrateInvest(scale, index)
+   {
+       let len = this._data.investment.length;
+       let sum = 0;
+       for (let i = 0; i < len; i++)
+       {
+           if (this._data.investment[i].Frequency == scale)
+               sum += this._integrated_investments[i][index];
+       }
+
+       return sum;
+   }
+
+   /**
+    * Compute year invest spences
+    *
+    * @return {Number} sum of year investition
+    */
+   computeYearInvest(index)
+   {
+     let len = this._data.investment.length;
+     let sum = 0;
+     let ind = 0;
+     for(let i = 0; i < len; i++)
+     {
+         if(this._data.investment[i].Frequency == "Year")
+         {
+           ind = (index/12) - 1;
+           if (index == 12)
+           {
+              this._integrated_investments[i][ind] = this._data.investment[i].Capital;
+              let increment = this._data.investment[i].Value*this._data.investment[i].Modifier;
+              this._integrated_investments[i][ind] += increment;
+              sum += increment;
+              this._integrated_investments[i][ind] += this.computeInterest(this._integrated_investments[i][ind], this._data.investment[i].Increase);
+           }
+           else
+           {
+              let increment = this._data.investment[i].Value*this._data.investment[i].Modifier;
+              this._integrated_investments[i][ind] = this._integrated_investments[i][ind-1] + increment;
+              sum += increment;
+              this._integrated_investments[i][ind] += this.computeInterest(this._integrated_investments[i][ind], this._data.investment[i].Increase);
+           }
+         }
+     }
+
+     return sum;
+   }
+   /**
+    * Compute monthly value of incomes and spends
+    *
+    * @param  {String} type type of batch
+    * @param  {String} scale time scale of batch
+    * @return {Number}      summation of all monthly items
+    */
+   computeBatch(type, scale)
+   {
+       let len = this._data[type].length;
+       let sum = 0;
+       for(let i = 0; i < len; i++)
+       {
+           if(this._data[type][i].Frequency == scale)
+              sum += this._data[type][i].Value*this._data[type][i].Modifier;
+       }
+
+      return sum;
+   }
+
+   /**
+    * Compute monthly invest spends
+    *
+    * @param  {Number} index index of current month
+    * @return {Number}       sum of monthly investition
+    */
+   computeMonthInvest(index)
+   {
+     let len = this._data.investment.length;
+     let sum = 0;
+     for(let i = 0; i < len; i++)
+     {
+         if(this._data.investment[i].Frequency == "Month")
+         {
+           let ind = index-1;
+           if (index == 1)
+           {
+              this._integrated_investments[i][ind] = this._data.investment[i].Capital;
+              let increment = this._data.investment[i].Value*this._data.investment[i].Modifier;
+              this._integrated_investments[i][ind] += increment;
+              sum += increment;
+           }
+           else
+           {
+              let increment = this._data.investment[i].Value*this._data.investment[i].Modifier;
+              this._integrated_investments[i][ind] = this._integrated_investments[i][ind-1] + increment;
+              sum += increment;
+              if (index % 12 == 0)
+                  this._integrated_investments[i][ind] += this.computeInterest(this._integrated_investments[i][ind], this._data.investment[i].Increase);
+           }
+         }
+     }
+
+     return sum;
+   }
+
+   /**
+    * Compute interest base on interest rate
+    *
+    * @param  {Number} value
+    * @param  {Number} interest interest rate
+    * @return {Number}          computed interest
+    */
+   computeInterest(value, interest)
+   {
+       return (value / 100)*interest;
    }
 }
 
--- test/computeengine.spec.js
+++ test/computeengine.spec.js
@@ -14,13 +14,15 @@ describe("Compute engine", ()=>
       let savings = 1000;
       data.spend = [];
       data.spend[0] = new Item("Food", 50, "Month", 1);
+      data.spend[1] = new Item("Holiday", 200, "Year", 1);
       data.income = [];
       data.income[0] = new Item("Salary", 500, "Month", 1);
       data.investment = [];
       data.investment[0] = new Invest("Pension Fund", 100, "Month", 1, 1000, 6);
+      data.investment[1] = new Invest("Luck",1000, "Year", 1, 0, 10);
       let ComputeEngine = require("../src/js/computeEngine");
       let noise = new Noise(true);
-      let range = 3;
+      let range = 2;
       engine = new ComputeEngine(range, savings, data, noise);
    });
 
@@ -33,5 +35,14 @@ describe("Compute engine", ()=>
        expect(engine).to.have.property("_month_ratio");
        expect(engine).to.have.property("_integrated_incomes");
        expect(engine).to.have.property("_integrated_investments");
+   });
+
+   it("Compute engine should compute", ()=>
+   {
+       engine.integrate();
+       expect(engine.IntegratedIncomes).to.deep.equal([1350, 1700, 2050, 2400, 2750, 3100, 3450, 3800, 4150, 4500, 4850, 4000, 4350, 4700, 5050, 5400, 5750, 6100, 6450, 6800, 7150, 7500, 7850, 7000]);
+       expect(engine.IntegratedInvestments[0]).to.deep.equal([1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000, 2100, 2332, 2432, 2532, 2632, 2732, 2832, 2932, 3032, 3132, 3232, 3332, 3432, 3743.92]);
+       expect(engine.IntegratedInvestments[1]).to.deep.equal([1100, 2310]);
+       expect(engine.IntegratedWhealth).to.deep.equal([2450, 2900, 3350, 3800, 4250, 4700, 5150, 5600, 6050, 6500, 6950, 7432, 7882, 8332, 8782, 9232, 9682, 10132, 10582, 11032, 11482, 11932, 12382, 13053.92]);
    });
 });
